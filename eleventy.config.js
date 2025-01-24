87a774702fd733e11bb4626c0725ab7b3341091c
Tiago Jacinto <tajacinto@gmail.com>
2025-01-24 22:35:51 +0000

    upd

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,4 @@
-export default function(eleventyConfig) {
+module.exports = function(eleventyConfig) {
   eleventyConfig.setInputDirectory("src");
   eleventyConfig.addPassthroughCopy("styles.css");
 };
