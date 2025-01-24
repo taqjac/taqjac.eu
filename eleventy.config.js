0eb846f7bc02d1a7d2f893b47e358ce52292b562
Tiago Jacinto <tajacinto@gmail.com>
2025-01-24 23:17:18 +0000

    added rss plugin

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,5 @@
 const { DateTime } = require("luxon");
+const { feedPlugin } = require("@11ty/eleventy-plugin-rss");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.setInputDirectory("src");
@@ -7,5 +8,22 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addFilter("formatDate", (date) => {
     return DateTime.fromJSDate(date).toFormat('yyyy-MM-dd');
   });
-  
+  eleventyConfig.addPlugin(feedPlugin, {
+		type: "atom", // or "rss", "json"
+		outputPath: "/feed.xml",
+		collection: {
+			name: "posts", // iterate over `collections.posts`
+			limit: 10,     // 0 means no limit
+		},
+		metadata: {
+			language: "en",
+			title: "Blog Title",
+			subtitle: "This is a longer description about your blog.",
+			base: "https://example.com/",
+			author: {
+				name: "Your Name",
+				email: "", // Optional
+			}
+		}
+	});
 };
